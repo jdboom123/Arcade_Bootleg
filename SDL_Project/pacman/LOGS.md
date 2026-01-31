97d6170c09e2a4786194155f5216264f93b9e2a7
jdboom123 <jarmedilla16@gmail.com>
2026-01-31 10:45:00 -0600

    pacman move manually

--- SDL_Project/pacman/LOGS.md
+++ SDL_Project/pacman/LOGS.md
@@ -4,3 +4,14 @@ Starting to work on a Pac- Man bootleg. Some things that I have noticed:
 - SDL_FRect will help me draw rectangles, which will be the placeholders for the sprites in Pac-Man
 
 Today's goal will be to draw a yellow square that can move around and consume small pellets
+
+# 1-31-2026: Getting Pac-Man to Move Around
+Trying to get Pac-Man to move across the screen by adjusting its x and y position using the arrow keys and then updating the renderer.
+
+Issue: Pac-Man stops rendering when it hits 128 pixels in both the x and y position
+- 7 bits of rendering(?)
+- Issue with the char data type (Only 8 bits, and since it was signed, we only had 7 bits to describe the numerical part)
+- Changed data type to int and changed logic in movement so that it wraps to the other side when going out of bounds
+
+Issue: Going all the way right would cause the sprite to be stuck at -15 in both the x and y direction. 
+- Fixed issue removing unsigned from the PACMAN_WINDOW_WIDTH and PACMAN_WINDOW_HEIGHT
--- SDL_Project/pacman/main.c
+++ SDL_Project/pacman/main.c
@@ -3,21 +3,24 @@
 #include <SDL3/SDL_main.h>
 
 #define PACMAN_BLOCK_SIZE_IN_PIXELS 16
+#define PACMAN_SPEED 4
 
-#define PACMAN_GAME_WIDTH 28U
-#define PACMAN_GAME_HEIGHT 36U
+#define PACMAN_GAME_WIDTH 28
+#define PACMAN_GAME_HEIGHT 36
 
 #define SDL_WINDOW_WIDTH           (PACMAN_BLOCK_SIZE_IN_PIXELS * PACMAN_GAME_WIDTH)
 #define SDL_WINDOW_HEIGHT          (PACMAN_BLOCK_SIZE_IN_PIXELS * PACMAN_GAME_HEIGHT)
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
+// static SDL_Window *window = NULL;
+// static SDL_Renderer *renderer = NULL;
 
 typedef struct
 {
-    unsigned char cells [PACMAN_GAME_WIDTH*PACMAN_GAME_HEIGHT];
-    char pacman_xpos;
-    char pacman_ypos;
+    int cells [PACMAN_GAME_WIDTH*PACMAN_GAME_HEIGHT];
+    int pacman_xpos;
+    int pacman_ypos;
+    int pacman_xpos_prev;
+    int pacman_ypos_prev;
 }PacManContext;
 
 typedef struct
@@ -34,6 +37,8 @@ void pacman_initialize(PacManContext *ctx)
     /* Change to appropiate locations later*/
     ctx->pacman_xpos = 0;
     ctx->pacman_ypos = 0;
+    ctx->pacman_xpos_prev = 0;
+    ctx->pacman_ypos_prev = 0;
 }
 
 // void snake_redir(SnakeContext *ctx, SnakeDirection dir)
@@ -47,21 +52,65 @@ void pacman_initialize(PacManContext *ctx)
 //     }
 // }
 
+static SDL_AppResult handle_key_event_(PacManContext *ctx, SDL_Scancode key_code)
+{
+    switch (key_code) {
+    /* Quit. */
+    case SDL_SCANCODE_ESCAPE:
+    case SDL_SCANCODE_Q:
+        return SDL_APP_SUCCESS;
+    /* Restart the game as if the program was launched. */
+    case SDL_SCANCODE_R:
+        pacman_initialize(ctx);
+        break;
+    /* Decide new direction of the snake. */
+    case SDL_SCANCODE_RIGHT:
+        ctx->pacman_xpos += PACMAN_SPEED;
+        if (ctx->pacman_xpos >= SDL_WINDOW_WIDTH){
+            ctx->pacman_xpos = -1*PACMAN_BLOCK_SIZE_IN_PIXELS + 1;
+        }
+        printf("%d\n",ctx->pacman_xpos);
+        break;
+    case SDL_SCANCODE_UP:
+        ctx->pacman_ypos -= PACMAN_SPEED;
+        if (ctx->pacman_ypos <= -1*PACMAN_BLOCK_SIZE_IN_PIXELS){
+            ctx->pacman_ypos = SDL_WINDOW_HEIGHT-1;
+        }
+        break;
+    case SDL_SCANCODE_LEFT:
+        ctx->pacman_xpos -= PACMAN_SPEED;
+        if (ctx->pacman_xpos <= -1*PACMAN_BLOCK_SIZE_IN_PIXELS){
+            ctx->pacman_xpos = SDL_WINDOW_WIDTH-1;
+        }
+        break;
+    case SDL_SCANCODE_DOWN:
+        ctx->pacman_ypos += PACMAN_SPEED;
+        if (ctx->pacman_ypos >= SDL_WINDOW_HEIGHT){
+            ctx->pacman_ypos = -1*PACMAN_BLOCK_SIZE_IN_PIXELS + 1;
+        }
+        break;
+    default:
+        break;
+    }
+    return SDL_APP_CONTINUE;
+}
+
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {   
-    /* Create the window */
-    if (!SDL_CreateWindowAndRenderer("BOOTLEG PAC-MAN", SDL_WINDOW_WIDTH, SDL_WINDOW_HEIGHT, 0, &window, &renderer)) {
-        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
-
     AppState *as = (AppState *)SDL_calloc(1, sizeof(AppState));
     if (!as) {
         return SDL_APP_FAILURE;
     }
 
     *appstate = as;
+
+    /* Create the window */
+    if (!SDL_CreateWindowAndRenderer("BOOTLEG PAC-MAN", SDL_WINDOW_WIDTH, SDL_WINDOW_HEIGHT, 0, &as->window, &as->renderer)) {
+        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetRenderLogicalPresentation(as->renderer, SDL_WINDOW_WIDTH, SDL_WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
     pacman_initialize(&as->pacman_ctx);
 
@@ -71,12 +120,12 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    // PacManContext *ctx = &((AppState *)appstate)-> pacman_ctx;
+    PacManContext *ctx = &((AppState *)appstate)-> pacman_ctx;
     switch(event->type){
         case SDL_EVENT_QUIT:
             return SDL_APP_SUCCESS;
-        // case SDL_EVENT_KEY_DOWN:
-        //     return handle_key_event_(ctx, event->key.scancode);
+        case SDL_EVENT_KEY_DOWN:
+            return handle_key_event_(ctx, event->key.scancode);
         default:
             break;
     }
@@ -92,17 +141,25 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     SDL_FRect r;
 
     // Set up background
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(as->renderer, 0, 0, 0, 255);
+    SDL_RenderClear(as->renderer);
 
     r.w = r.h = PACMAN_BLOCK_SIZE_IN_PIXELS;
 
     // Draw Pac-Man
-    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-    r.x = r.y = 0;
-    SDL_RenderFillRect(renderer, &r);
+    SDL_SetRenderDrawColor(as->renderer, 255, 255, 0, 255);
+    r.x = ctx->pacman_xpos;
+    r.y = ctx->pacman_ypos;
+    SDL_RenderFillRect(as->renderer, &r);
 
-    SDL_RenderPresent(renderer);
+    // if (ctx->pacman_xpos != ctx->pacman_xpos_prev || ctx->pacman_ypos != ctx->pacman_ypos_prev){
+    //     printf("%f , %f\n", r.x, r.y);
+    // }
+    ctx->pacman_xpos_prev = ctx->pacman_xpos;
+    ctx->pacman_ypos_prev = ctx->pacman_ypos;
+
+
+    SDL_RenderPresent(as->renderer);
     return SDL_APP_CONTINUE;
 }
 
